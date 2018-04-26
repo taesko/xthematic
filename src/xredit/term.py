e5c1259f52943ebbe3e59c332f0e6d29d4e95ee6
Antonio Todorov <taeskow@gmail.com>
2018-04-26 21:31:58 +0300

    Refactor term.py to always store updated custom and loaded colors.
    Refactored the loaded_colors() and *_customized_colors() functions into two classes that are meant to be used as singletons:
    _LoadedColors() with instance LOADED_COLORS and _CustomColors() with instance CUSTOM_COLORS
    Both classes implement an abc.Mapping interface and automatically check if the custom or loaded colors have changed externally.
    
    This commit also adds logging to the module.

--- src/xredit/term.py
+++ src/xredit/term.py
@@ -1,72 +1,147 @@
 import functools
-
-import collections
-import contextlib
 import json
+import logging
 import re
 import subprocess
+
+import collections
 
 import xredit.colors
 import xredit.config
 
-
-def loaded_colors():
-    queried = subprocess.Popen(['xrdb', '-query'], stdout=subprocess.PIPE)
-    grepped = subprocess.check_output(['grep', 'color'], stdin=queried.stdout)
-    queried.wait()
-    lines = grepped.splitlines()
-    matches = (re.match(pattern=b'.*color(\d+):\t([^ ]+)', string=l) for l in lines)
-    grouped = (m.groups() for m in matches)
-    cast = ((int(num), byte_arr.decode(encoding='ascii')) for num, byte_arr in grouped)
-    colors = {}
-    for number, hex_code in sorted(cast):
-        if number in colors and hex_code != colors[number]:
-            raise RuntimeError(f"color{number} has more than one value")
-        cid = xredit.colors.ColorIdentifier(number)
-        c = xredit.colors.Color(hex_code)
-        colors[cid] = c
-    return colors  # values are sorted by keys
+logger = logging.getLogger(__name__)
 
 
-def read_customized_colors():
-    with xredit.config.USER_CUSTOM_FILE.open() as f:
-        json_dict = json.load(f)
-    color_strings = json_dict.get(xredit.config.TERMINAL_SESSION_ID, {})
-    colors = {}
-    for index, hex_code in color_strings.items():
-        index = int(index)
-        colors[xredit.colors.ColorIdentifier(index)] = xredit.colors.Color(hex_code)
-    return colors
+def keep_updated(method):
+    @functools.wraps(method)
+    def new_method(self, *args, **kwargs):
+        if self.is_outdated():
+            self.update()
+        return method(self, *args, **kwargs)
+
+    return new_method
 
 
-def save_custom_color(color_id, color):
-    with xredit.config.USER_CUSTOM_FILE.open(mode='r') as f:
-        json_dict = json.load(f)
-    color_hexes = json_dict.get(xredit.config.TERMINAL_SESSION_ID, {})
-    color_hexes[str(color_id.id)] = str(color.hex)
-    json_dict[xredit.config.TERMINAL_SESSION_ID] = color_hexes
-    with xredit.config.USER_CUSTOM_FILE.open(mode='w') as f:
-        json.dump(obj=json_dict, fp=f)
+class _LoadedColors(collections.Mapping):
+
+    def __init__(self):
+        self._colors = {}
+        self._hash = ''
+        logger.debug('initialized %s instance %s', self.__class__.__name__, self)
+
+    @staticmethod
+    def xrdb_hash():
+        return hash(subprocess.check_output(['xrdb', '-query']))
+
+    @staticmethod
+    def loaded():
+        queried = subprocess.Popen(['xrdb', '-query'], stdout=subprocess.PIPE)
+        grepped = subprocess.check_output(['grep', 'color'], stdin=queried.stdout)
+        queried.wait()
+        lines = grepped.splitlines()
+        matches = (re.match(pattern=b'.*color(\d+):\t([^ ]+)', string=l) for l in lines)
+        grouped = (m.groups() for m in matches)
+        cast = ((int(num), byte_arr.decode(encoding='ascii')) for num, byte_arr in grouped)
+        colors = {}
+        for number, hex_code in sorted(cast):
+            if number in colors and hex_code != colors[number]:
+                raise RuntimeError(f"color{number} has more than one value")
+            cid = xredit.colors.ColorIdentifier(number)
+            c = xredit.colors.Color(hex_code)
+            colors[cid] = c
+        return colors  # values are sorted by keys
+
+    @keep_updated
+    def __iter__(self):
+        yield from self._colors
+
+    @keep_updated
+    def __len__(self):
+        return len(self._colors)
+
+    @keep_updated
+    def __getitem__(self, k):
+        return self._colors[k]
+
+    def update(self):
+        # print(self)
+        # logger.info('updating _LoadedColors instance %s', self)
+        self._colors = self.loaded()
+        self._hash = self.xrdb_hash()
+        # logger.info('loaded color identifiers: %s', [c.id for c in self._colors])
+
+    def is_outdated(self):
+        return self._hash != self.xrdb_hash()
 
 
-def remove_custom_color(color_id):
-    with xredit.config.USER_CUSTOM_FILE.open(mode='r') as f:
-        json_dict = json.load(f)
-    color_hexes = json_dict.get(xredit.config.TERMINAL_SESSION_ID, {})
-    color = color_hexes[str(color_id.id)]
-    del color_hexes[str(color_id.id)]
-    json_dict[xredit.config.TERMINAL_SESSION_ID] = color_hexes
-    with xredit.config.USER_CUSTOM_FILE.open(mode='w') as f:
-        json.dump(obj=json_dict, fp=f)
+LOADED_COLORS = _LoadedColors()
 
 
-def reset_custom_colors():
-    with xredit.config.USER_CUSTOM_FILE.open(mode='r') as f:
-        json_dict = json.load(f)
-    if xredit.config.TERMINAL_SESSION_ID in json_dict:
-        del json_dict[xredit.config.TERMINAL_SESSION_ID]
-    with xredit.config.USER_CUSTOM_FILE.open(mode='w') as f:
-        json.dump(obj=json_dict, fp=f)
+class _CustomColors(collections.MutableMapping):
+    def __init__(self, session_id=xredit.config.TERMINAL_SESSION_ID):
+        self._session_id = session_id
+        self._colors = self.read_customized_colors(self._session_id)
+        logger.debug('initialized %s instance %s', self.__class__.__name__, self)
+        logging.info('custom colors are %s', self._colors)
+
+    @staticmethod
+    def custom_dict():
+        with xredit.config.USER_CUSTOM_FILE.open() as f:
+            return json.load(f)
+
+    @staticmethod
+    def read_customized_colors(session_id=xredit.config.TERMINAL_SESSION_ID):
+        json_dict = _CustomColors.custom_dict()
+        color_strings = json_dict.get(session_id, {})
+        colors = {}
+        for index, hex_code in color_strings.items():
+            index = int(index)
+            colors[xredit.colors.ColorIdentifier(index)] = xredit.colors.Color(hex_code)
+        return colors
+
+    def __len__(self) -> int:
+        return len(self._colors)
+
+    def __iter__(self):
+        yield from self._colors
+
+    def __getitem__(self, item):
+        return self._colors[item]
+
+    def __setitem__(self, color_id, color):
+        json_dict = self.__class__.custom_dict()
+        color_hexes = json_dict.get(self._session_id, {})
+        color_hexes[str(color_id.id)] = str(color.hex)
+        json_dict[xredit.config.TERMINAL_SESSION_ID] = color_hexes
+        with xredit.config.USER_CUSTOM_FILE.open(mode='w') as f:
+            json.dump(obj=json_dict, fp=f)
+        self._colors[color_id] = color
+        logger.info('set custom color %s to %s', color_id, color)
+
+    def __delitem__(self, color_id):
+        json_dict = self.__class__.custom_dict()
+        color_hexes = json_dict.get(self._session_id, {})
+        color = color_hexes[str(color_id.id)]
+        assert color == self._colors[color_id].hex
+        del color_hexes[str(color_id.id)]
+        json_dict[xredit.config.TERMINAL_SESSION_ID] = color_hexes
+        with xredit.config.USER_CUSTOM_FILE.open(mode='w') as f:
+            json.dump(obj=json_dict, fp=f)
+        logger.info('removed custom color %s with hex %s', color_id, color)
+        del self._colors[color_id]
+
+    def reset(self):
+        json_dict = self.__class__.custom_dict()
+        if self._session_id in json_dict:
+            del json_dict[self._session_id]
+        with xredit.config.USER_CUSTOM_FILE.open(mode='w') as f:
+            json.dump(obj=json_dict, fp=f)
+        logger.info('reset all custom colors')
+        logger.info('removed colors: ', self._colors)
+        self._colors.clear()
+
+
+CUSTOM_COLORS = _CustomColors()
 
 
 class _TermColors(collections.MutableMapping):
@@ -74,10 +149,8 @@ class _TermColors(collections.MutableMapping):
 
     def __init__(self):
         # TODO include defaults for missing customized colors
-        # TODO make this object a constantly updating view/model
-        self.loaded = loaded_colors()
-        # TODO make this object a constantly updating view/model of the json file
-        self.custom = read_customized_colors()
+        self.loaded = LOADED_COLORS
+        self.custom = CUSTOM_COLORS
         self.colors = DictView(self.loaded, self.custom)
 
     def __iter__(self):
@@ -91,33 +164,25 @@ class _TermColors(collections.MutableMapping):
 
     def __setitem__(self, color_id, color):
         if self.colors[color_id] == color:
-            print(color_id, 'is already set')
+            logger.debug('%s is already set to %s', color_id, color)
             return
 
         index = str(color_id.id)
         r, g, b = map(str, color.rgb_large_percentage)
         initc = subprocess.run(['tput', 'initc', index, r, g, b])
         initc.check_returncode()
+        logger.info('set terminal color %s to %s', color_id, color)
         if self.loaded[color_id] == color and color_id in self.custom:
-            print('deleting custom color', self.custom[color_id])
-            remove_custom_color(color_id)
             del self.custom[color_id]
         elif self.loaded[color_id] == color:
-            assert False, f'{color} is not a custom color, but previously {color_id} was overwrited in loaded'
+            msg = f'{color} is not a custom color, but previously {color_id} was overwrited in loaded'
+            logger.critical(msg)
+            assert False, msg
         else:
             self.custom[color_id] = color
-            save_custom_color(color_id, color)
 
     def __delitem__(self, color_id):
         raise NotImplementedError()
-
-    @contextlib.contextmanager
-    def tryout(self, color_id, color):
-        """ Swap the hex code of a color_id for the open terminal with a new one during the context."""
-        old_color = self[color_id]
-        self[color_id] = color
-        yield
-        self[color_id] = old_color
 
     def __repr__(self):
         return "{self.__class__}({colors})".format(
